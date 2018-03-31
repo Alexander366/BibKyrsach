79e93192d2f46405df0cfa66e52ba88d8e6402a1
Alexander366 <31627051+Alexander366@users.noreply.github.com>
2018-03-31 14:37:20 +0300

    Add files via upload
    Первый код

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib> // для system
+using namespace std;
+
+class Kniga{
+	string name;
+	string Ganr;
+	string Izdatel;
+	int seriya;
+};
+
+int main() 
+{ 	
+    cout << "Hello, world!" << endl;
+    system("pause"); // Только для тех, у кого MS Visual Studio
+    return 0; 
+}
